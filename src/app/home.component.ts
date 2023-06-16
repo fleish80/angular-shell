29cf60eedc9d2e56d9ac76dcb49565ea75546c00
fleish80 <Fleish.D@gmail.com>
2023-06-16 14:13:02 +0300

    shared services

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CustomManifest, PluginOptions } from './mf.model';
 import { getManifest } from '@angular-architects/module-federation';
-import { ShareLibService } from 'share-lib';
+import { ShareLibService, SharedPlatformService, SharedRootService } from 'share-lib';
 
 @Component({
   selector: 'app-home',
@@ -14,22 +14,38 @@ import { ShareLibService } from 'share-lib';
       </div>
     </div>
 
-    <button (click)="sendData()"> Send data to remote mfe</button>
-
-    {{name$ | async}}
+    <ul>
+      <li><button (click)="sendData()">Send data to shared service</button></li>
+      <li>
+        <button (click)="sendText()">Send data to platform service</button>
+      </li>
+      <li><button (click)="sendToRoot()">Send data to root service</button></li>
+    </ul>
+    <ul>
+      <li>{{ sharedText$ | async }}</li>
+      <li>{{ platformText$ | async }}</li>
+      <li>{{ rootText$ | async }}</li>
+    </ul>
   `,
   styles: [],
 })
 export class HomeComponent implements OnInit {
   options: PluginOptions[] = [];
-  name$ = this.shareLib.name$;
+  
+  #shareLibService = inject(ShareLibService);
+  #sharedPlatformService = inject(SharedPlatformService);
+  #sharedRootService = inject(SharedRootService);
+
+  sharedText$ = this.#shareLibService.text$;
+  platformText$ = this.#sharedPlatformService.text$;
+  rootText$ = this.#sharedRootService.text$;
+
 
 
   constructor(private readonly shareLib: ShareLibService) {}
 
   ngOnInit(): void {
     const manifest = getManifest<CustomManifest>();
-
 
     // filter remote mfe's which needs to loaded inside page (not via route)
     this.options = Object.values(manifest).filter(
@@ -38,6 +54,14 @@ export class HomeComponent implements OnInit {
   }
 
   sendData() {
-    this.shareLib.addName('shell');
+    this.#shareLibService.addName('mfe shell');
+  }
+
+  sendText() {
+    this.#sharedPlatformService.addName('mfe shell');
+  }
+
+  sendToRoot() {
+    this.#sharedRootService.addName('mfe shell');
   }
 }
